e1f075d2f47833a11cbb0eb0d0d752af3e4ce191
parth <parths@itpathsolutions.co.in>
2022-03-07 18:16:38 +0530

    Make menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 
-import {Article,Brand,CTA,Navbar} from './components/index'
-import {Features,Header,Blog,Possibility} from './containers/index'
-const App=()=> {
+import { Article, Brand, CTA, Navbar } from './components/index'
+import { Features, Header, Blog, Possibility } from './containers/index'
+const App = () => {
   return (
     <div className="App">
-   <div className="gradient_bg">
-     <Navbar/>
-     <Header/>
-
-     </div>
-     <Brand/>
-     <Features/>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Features />
+      <CTA />
+      <Blog />
     </div>
   );
 }
