7a7713f9bd04d447cc5f202054a2ca763b76bc35
parth <parths@itpathsolutions.co.in>
2022-03-07 14:41:50 +0530

    make folder directory

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './brand.css'
+const Brand=()=> {
+  return (
+    <div>Arcticle</div>
+  )
+}
+
+export default Brand
---
+++ src/containers/index.jsx
@@ -0,0 +1,4 @@
+export {default as Blog} from './blog'
+export {default as Features} from './features'
+export {default as Header} from './header'
+export {default as Possibility} from './possibility'
