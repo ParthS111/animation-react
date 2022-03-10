a6a10202a5f7dec2d87be23f18c4b8991d99289a
parth <parths@itpathsolutions.co.in>
2022-03-10 14:29:10 +0530

    create brand

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
 import { Article, Brand, CTA, Navbar } from './components/index'
 import { Features, Header, Blog, Possibility } from './containers/index'
 const App = () => {
+
+  useEffect(() => {
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+    });
+  },[])
   return (
     <div className="App">
       <div className="gradient__bg">
--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import './brand.css'
+import {google,slack,shopify,atlassian} from './imports.js'
 const Brand=()=> {
   return (
-    <div>Arcticle</div>
+    <div className='gtp3_branch section__padding'>
+      <div className=''>
+        <img src={google} alt="google"/>
+      </div>
+      <div className=''>
+        <img src={slack} alt="slack"/>
+      </div>
+      <div className=''>
+        <img src={shopify} alt="shopify"/>
+      </div>
+      <div className=''>
+        <img src={atlassian} alt="atlassian"/>
+      </div>
+
+    </div>
   )
 }
 
